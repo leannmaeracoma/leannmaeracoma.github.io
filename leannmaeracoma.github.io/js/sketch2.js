6b1dc63d2a44250522ca8bc29f258e8b56fa3c40
LeAnn Mae Racoma <37121557+leannmaeracoma@users.noreply.github.com>
2018-05-10 09:37:16 -0700

    Processing page

--- leannmaeracoma.github.io/js/sketch2.js
+++ leannmaeracoma.github.io/js/sketch2.js
@@ -35,7 +35,7 @@ function star(x, y, radius1, radius2, npoints) {
   var angle = TWO_PI / npoints;
   var halfAngle = angle/2.0;
   beginShape();
-  for (var a = 0; a < TWO_PI; a += angle) {
+  for (let a = 0; a < TWO_PI; a += angle) {
     var sx = x + cos(a) * radius2;
     var sy = y + sin(a) * radius2;
     vertex(sx, sy);
